e95f477b68cf853501d5c4489d5dcf1adcd768ad
whyyrohith <srohith976@gmail.com>
2024-09-26 12:22:04 +0530

    Add files via upload

---
+++ okk html/script.js
@@ -0,0 +1,14 @@
+function operation(a,b,c{
+    let d = a + b;
+    d = d * c;
+    d = d/ (d+a;
+        let result = d+a-b/;
+        return result;
+        var a=10;
+        var b=20;
+        if(a>b){
+            document.getElementryId("demo").innerHTML ="a equal to b";
+    
+        } 
+        else{
+            document.getElementrybyId("demo").innerHTML
